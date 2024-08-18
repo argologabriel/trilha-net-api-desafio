55842cd5a35027d18d75efec3d35861c81557f4a
Gabriel Argôlo <gabriel.argolo@outlook.com>
2024-08-18 18:14:24 -0300

    feat: implementação do método GET ObterPorTitulo

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -44,8 +44,10 @@ namespace TrilhaApiDesafio.Controllers
         public IActionResult ObterPorTitulo(string titulo)
         {
             // DONE: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
+
+            var tarefa = _context.Tarefas.Where(x => x.Titulo.Contains(titulo));
+
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterPorData")]
